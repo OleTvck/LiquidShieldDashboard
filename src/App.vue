2cf38ed65fa50da9e9b4133a69ec32c451ee2591
Jussi <jussi.pesonen@adaptek.fi>
2022-06-10 23:10:29 +0300

    Added pinia store to the project. Cleanup and data flow advancements.

--- src/App.vue
+++ src/App.vue
@@ -5,45 +5,56 @@
   </div>
 </template>
 
-<script setup>
-  import Sidebar from './components/SideBar.vue'
+<script>
+import { useStore } from './store/cveStore';
+import Sidebar from './components/SideBar.vue';
+
+export default {
+  components: { Sidebar },
+  mounted() {
+    // This is run when the whole app is first created, ie. before anything.
+    this.$nextTick(() => {
+      const store = useStore();
+      store.fetchScores();
+    });
+  },
+};
 </script>
 
 <style lang="scss">
-
 :root {
-	--primary: #4acdde;
-	--primary-alt: #22c55e;
-	--grey: #64748b;
-	--dark: #1e293b;
-	--dark-alt: #334155;
-	--light: #f1f5f9;
-	--sidebar-width: 300px;
+  --primary: #4acdde;
+  --primary-alt: #22c55e;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px;
 }
 * {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'Fira sans', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira sans', sans-serif;
 }
 body {
-	background: var(--light);
+  background: var(--light);
 }
 button {
-	cursor: pointer;
-	appearance: none;
-	border: none;
-	outline: none;
-	background: none;
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
 }
 .app {
-	display: flex;
-	main {
-		flex: 1 1 0;
-		padding: 2rem;
-		@media (max-width: 1024px) {
-			padding-left: 6rem;
-		}
-	}
+  display: flex;
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+    @media (max-width: 1024px) {
+      padding-left: 6rem;
+    }
+  }
 }
 </style>
